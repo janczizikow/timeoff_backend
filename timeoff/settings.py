ccd4f804e5a4557ee3cd2e605b2cdcea0d6be246
Jan <jan.czizikow@gmail.com>
2019-11-30 20:51:49 +0100

    bump

--- timeoff/settings.py
+++ timeoff/settings.py
@@ -22,17 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-try:
-    DEBUG = os.environ['DEBUG'] == 'True'
-except KeyError:
-    DEBUG = True
+DEBUG_ENV = os.getenv('DEBUG', 'True')
+
+DEBUG = DEBUG_ENV == 'True'
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if DEBUG:
-    SECRET_KEY = 'secret'
-else:
-    SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 
 ALLOWED_HOSTS = []
 
